bde3bc456de8e2717c0804cb86c4f84a30bf2f07
mdmourao <martimmourao@gmail.com>
2026-09-09 07:42:52 +0100

    Add test files with intentional errors for ESLint and stylelint

---
+++ teste-erros.js
@@ -0,0 +1,17 @@
+// Ficheiro com erros propositados para testar o ESLint.
+
+function saudacao(nome) {
+  console.log("Olá " + nomee);
+}
+
+const config = {
+  cor: "azul",
+  cor: "verde",
+};
+
+var contador = 0;
+var contador = 1;
+
+const naoUsado = 42;
+
+saudacao("turma");
